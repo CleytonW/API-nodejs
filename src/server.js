329a94fd220572a5f4a3ef79bc565de5ddcf5976
CleytonW <cleytonw11@gmail.com>
2023-03-16 09:04:44 -0300

    test query parameters

--- src/server.js
+++ src/server.js
@@ -6,8 +6,17 @@ app.get("/message/:id/:user", (request, response) => {
   const { id, user } = request.params;
 
   response.send(`
-  Mensagem ID: ${id}.
-  Para o usário: ${user}.
+    Message ID: ${id}.
+    For user: ${user}.
+  `);
+});
+
+app.get("/users", (request, response) => {
+  const { page, limit } = request.query;
+
+  response.send(`
+    Page: ${page}.
+    Show: ${limit}.
   `);
 });
 
