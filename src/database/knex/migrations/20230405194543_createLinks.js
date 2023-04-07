96886323e6ada7917afeedc15cc3f76d3ff9919a
CleytonW <cleytonw11@gmail.com>
2023-04-07 16:18:52 -0300

    filter tags and notes

--- src/database/knex/migrations/20230405194543_createLinks.js
+++ src/database/knex/migrations/20230405194543_createLinks.js
@@ -4,7 +4,7 @@ exports.up = knex => knex.schema.createTable("links", table => {
   
 
   table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE");
-  table.timestamp("create_at").default(knex.fn.now());
+  table.timestamp("created_at").default(knex.fn.now());
 });
 
 exports.down = knex => knex.schema.dropTable("links");
